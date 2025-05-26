aaecdd5a324ec4abc2bba6545ea10c61bf4f58d6
Este4 <ledangminh1206@gmail.com>
2025-05-26 11:46:10 +0700

    add particles

---
+++ app.js
@@ -0,0 +1,30 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 50,
+      density: { enable: true, value_area: 800 },
+    },
+    color: { value: "#9932CC" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5, random: true },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#9932CC",
+      opacity: 0.2,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      out_mode: "out",
+    },
+  },
+});
